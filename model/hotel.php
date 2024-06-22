94930ee02c17a47e430527b1dbf91e688986485a
Salmakhl <147110378+Salmakhl@users.noreply.github.com>
2024-06-22 02:41:55 +0200

    Update hotel.php

--- model/hotel.php
+++ model/hotel.php
@@ -1,1 +1,12 @@
+<?php
 
+function db_conx(){
+  return new PDO('mysql:host=localhost; dbname=test','root','');
+}
+
+function liste_hotel(){
+    $connexion= db_conx();
+    $requet  = $connexion ->query("SELECT id_hotel  , titre, adresse,nbre_etoile FROM h ;");
+    return $requet;
+}
+?>
